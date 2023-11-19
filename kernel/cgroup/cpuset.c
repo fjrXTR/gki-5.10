e419f7ac67e4246597752f4156e3feb8d5c8cc78
Rama Bondan Prakoso <ramanarubp@gmail.com>
2023-11-20 06:47:54 +0700

    kernel: Fine tune cpusets assist values
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2590,9 +2590,9 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 		{ "foreground",		"0-5" },
 		{ "background",		"0-2" },
 		{ "system-background",	"0-3" },
-		{ "restricted",		"0-5" },
+		{ "restricted",		"0-2" },
 		{ "top-app",		"0-7" },
-		{ "camera-daemon",	"0-3,6-7" },
+		{ "camera-daemon",	"0-7" },
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
