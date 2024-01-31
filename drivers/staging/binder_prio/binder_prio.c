981242f5681880cf9826ede2496fff3b38da2606
Rama Bondan Prakoso <ramanarubp@gmail.com>
2024-02-01 02:25:12 +0700

    drivers: staging: binder_prio: Increase the priority for XOSLauncher
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- drivers/staging/binder_prio/binder_prio.c
+++ drivers/staging/binder_prio/binder_prio.c
@@ -12,6 +12,7 @@ static const char *task_name[] = {
 	"com.miui.home",
 	".globallauncher",  // com.mi.android.globallauncher
 	"droid.launcher3",  // com.android.launcher3
+	"ion.XOSLauncher", // com.transsion.XOSLauncher
 	"ndroid.systemui",  // com.android.systemui
 	// "surfaceflinger",
 	"com.tencent.mm",
